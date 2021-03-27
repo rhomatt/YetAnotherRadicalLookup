de4f6a4c0d56536d598a9d82f4b0c0244688d207
Matthew Rho <mcr9754@rit.edu>
2021-03-27 04:33:31 -0400

    need to get the kanji metadata from here

--- yarl/yarl/settings.py
+++ yarl/yarl/settings.py
@@ -11,9 +11,13 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import PurePath
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+ROOT_DIR = BASE_DIR.parent
+print("ROOT_DIR", ROOT_DIR)
 
 
 # Quick-start development settings - unsuitable for production
@@ -78,7 +82,7 @@ WSGI_APPLICATION = 'yarl.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ROOT_DIR / PurePath('kanji_metadata', 'wnjpn.db'),
     }
 }
 
