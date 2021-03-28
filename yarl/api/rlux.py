b8bde4c0f5d8db9d95da5b67322b39a60305a365
Matthew Rho <mcr9754@rit.edu>
2021-03-28 05:02:33 -0400

    rlux now generates an appropriate query

--- yarl/api/rlux.py
+++ yarl/api/rlux.py
@@ -8,37 +8,25 @@ def printjp(string):
     sys.stdout.buffer.write(out.encode('utf8'))
 
 class Block:
-    def __init__(self, block):
-        printjp(block)
+    def __init__(self, block, pos):
         self.minstrokes = None
         self.maxstrokes = None
         self.leniency = 0
+        self.pos = pos
         # http://www.localizingjapan.com/blog/2012/01/20/regular-expressions-for-japanese-text/
         # found information on japanese unicode here 
-        self.kanji = set(re.findall(u"[\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A]", block))
-        self.radicals = set()
+        count = 0
+        self.kanji = {}
+        for k in re.findall(u"[\u3400-\u4DB5\u4E00-\u9FCB\uF900-\uFA6A]", block):
+            self.kanji[k] = ':p' + str(pos) + 'r' + str(count)
+            count += 1
+            # we will need these for queries later to avoid SQL injection
         
-        find_radicals_query = "SELECT r.radical FROM Radicals r LEFT JOIN Krad kr ON r.radical = kr.radical WHERE\n"
-        rad_from_kanji = "kr.kanji = %s AND %s NOT IN (SELECT radical FROM Radicals)\n"
-
-        for k in self.kanji:
-            find_radicals_query += rad_from_kanji
-            if rad_from_kanji[:2] != "OR":
-                rad_from_kanji = "OR " + rad_from_kanji # each subsequent time, there will be an OR added
-
-        for k in self.kanji:
-            find_radicals_query += "UNION SELECT %s\n"
-        find_radicals_query += "EXCEPT SELECT k.kanji FROM Kanji k LEFT JOIN Radicals r2 ON k.kanji = r2.radical WHERE r2.radical IS NULL;"
-        printjp(find_radicals_query)
-
-        # QUERY DB HERE
-
-        # TODO replace japanese IME symbols with standard symbols
-
         # find max and minstrokes (if applicable)
         maxstrokes = re.match(r"-\d+", block)
         if maxstrokes:
             self.maxstrokes = int(maxstrokes.group(0)[1:])
+
         minstrokes = re.match(r"\d+-", block)
         if minstrokes:
             self.minstrokes = int(minstrokes.group(0)[:-1])
@@ -46,6 +34,32 @@ class Block:
         leniency = re.match(r"[Ll]\d+", block)
         if leniency:
             self.leniency = int(leniency.group(0)[1:])
+
+        find_radicals_query = "WITH rads AS (SELECT r.radical FROM Radical r LEFT JOIN Krad kr ON r.radical = kr.radical WHERE\n"
+        rad_from_kanji = "kr.kanji = %s AND %s NOT IN (SELECT radical FROM Radical)\n"
+
+        for k in self.kanji:
+            find_radicals_query += (rad_from_kanji % (self.kanji[k], self.kanji[k]))
+            if rad_from_kanji[:2] != "OR":
+                rad_from_kanji = "OR " + rad_from_kanji # each subsequent time, there will be an OR added
+
+        for k in self.kanji:
+            find_radicals_query += ("UNION SELECT %s\n" % self.kanji[k])
+
+        find_radicals_query += "EXCEPT SELECT kanji FROM Kanji LEFT JOIN Radical ON kanji = radical WHERE radical IS NULL)"
+        find_radicals_query += """
+            SELECT k.kanji
+            FROM Radical r, (SELECT COUNT(*) givenrads FROM rads)
+            LEFT JOIN Krad kr
+            ON kr.radical = r.radical
+            LEFT JOIN Kanji k ON kr.kanji = k.kanji
+            WHERE r.radical in rads
+            GROUP BY k.kanji
+            HAVING COUNT(r.radical) >= givenrads
+        """
+        self.radicals = find_radicals_query
+
+        # TODO replace japanese IME symbols with standard symbols
 
 
 
@@ -55,12 +69,9 @@ class Rlux:
         self.querystr = self.__create_query_str(exp)
         self.blockexp = None # root
         self.blocks = []
-        cur = None
-        blocks = re.findall(r'\(\w*\)', exp)
+        blocks = re.finditer(r'\(\w*\)', exp)
         for block in blocks:
-            self.blocks.append(Block(block))
-            
-
+            self.blocks.append(Block(block.group(0), block.start(0) + 1))
 
 
     # from the expression, creates the string we will use directly in our query
@@ -71,17 +82,31 @@ class Rlux:
         return querystr
         
 
-    def generate_query():
+    # returns the query we need, and the the variable info
+    def generate_query(self):
         query = """
-        SELECT w.lemma
-        FROM word w
-        WHERE w.lemma LIKE %s
-        """ % self.querystr
+            SELECT w.lemma
+            FROM word w 
+            WHERE w.lemma LIKE :querystr
+        """
 
-        
+        var_to_kanji = {"querystr": self.querystr}
+        if self.blocks:
+            sub_query = """
+                AND SUBSTR(w.lemma, %d, 1)  IN (
+                %s
+                )\n
+            """
+            
+            for block in self.blocks:
+                query += sub_query % (block.pos, block.radicals)
+                for kanji,vname in block.kanji.items():
+                    var_to_kanji[vname] = kanji
+
 
         query += ';'
-        return query
+
+        return query, var_to_kanji
     
     # given a set of kanji to look at, return only the kanji that match the exp
     def search(self, adict):
@@ -108,7 +133,13 @@ class Rlux:
 
 
 if __name__ == "__main__":
-    exp = Rlux("高(木父校)")
+    exp = Rlux("高(木父)")
+    query, vrs = exp.generate_query()
+    printjp(query)
+    for var, kanji in vrs.items():
+        printjp(kanji)
+
     exp = Rlux("(込)")
+    exp = Rlux("(込)(道)")
     exp = Rlux("(込道)")
 
