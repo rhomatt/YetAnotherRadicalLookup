c6889ed5a290461e692dcf932fc3e34534d6b7eb
yasumori <ysmr@protonmail.com>
2021-06-22 22:40:03 -0400

    moving stuff into their own urls

--- yarl/frontend/src/components/App.js
+++ yarl/frontend/src/components/App.js
@@ -1,6 +1,23 @@
 import React from 'react';
 import HomePage from './HomePage';
+import About from './About';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
 
 export default function App(){
-    return <HomePage/>
+    return (
+        <Router>
+            <Switch>
+                <Route path='/About'>
+                    <About/>
+                </Route>
+                <Route path='/'>
+                    <HomePage/>
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
--- yarl/frontend/src/components/SideBar.js
+++ yarl/frontend/src/components/SideBar.js
@@ -4,6 +4,9 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
 import GroupOutlinedIcon from '@material-ui/icons/GroupOutlined';
 import MonetizationOnOutlinedIcon from '@material-ui/icons/MonetizationOnOutlined';
+import {
+    Link,
+} from 'react-router-dom';
 
 export default class SideBar extends React.Component {
 	constructor(props){
@@ -17,41 +20,43 @@ export default class SideBar extends React.Component {
 	render(){
 		let toggled = this.props.toggled;
 		return (
-			<Drawer
-			varient="persistent"
-			anchor="left"
-			open={toggled}
-			>
-				<IconButton style={{marginLeft: "50%"}}edge="end" onClick={() => this.toggleMenu(false)}>
-					<ChevronLeftIcon/>
-				</IconButton>
-				<Divider/>
-				<List>
-					{ 
-						[
-							{
-								icon: <InfoOutlinedIcon/>, 
-								tag: "About",
-							}, 
-							{
-								icon: <GroupOutlinedIcon/>, 
-								tag: "Acknowledgements",
-							}, 
-							{
-								icon: <MonetizationOnOutlinedIcon/>, 
-								tag: "Donate",
-							}, 
-						].map(item =>
-							<ListItem button key={item.tag}>
-								<ListItemIcon>
-									{item.icon}
-								</ListItemIcon>
-								<ListItemText primary={item.tag}/>
-							</ListItem>
-						)
-					}
-				</List>
-			</Drawer>
+            <Drawer
+            varient="persistent"
+            anchor="left"
+            open={toggled}
+            >
+                <IconButton style={{marginLeft: "50%"}}edge="end" onClick={() => this.toggleMenu(false)}>
+                    <ChevronLeftIcon/>
+                </IconButton>
+                <Divider/>
+                <List>
+                    { 
+                        [
+                            {
+                                icon: <InfoOutlinedIcon/>, 
+                                tag: "About",
+                            }, 
+                            {
+                                icon: <GroupOutlinedIcon/>, 
+                                tag: "Acknowledgements",
+                            }, 
+                            {
+                                icon: <MonetizationOnOutlinedIcon/>, 
+                                tag: "Donate",
+                            }, 
+                        ].map(item =>
+                            <Link to={item.tag}>
+                                <ListItem button key={item.tag}>
+                                    <ListItemIcon>
+                                        {item.icon}
+                                    </ListItemIcon>
+                                    <ListItemText primary={item.tag}/>
+                                </ListItem>
+                            </Link>
+                        )
+                    }
+                </List>
+            </Drawer>
 		);
 	}
 }
